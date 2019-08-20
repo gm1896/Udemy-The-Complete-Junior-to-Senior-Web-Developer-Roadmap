710a79e79a9803c14fe0a60ba356b16f032faed1
Gaurav Mahadik <gaurav1896.gm@gmail.com>
2019-08-20 18:09:55 -0400

    Added AWS section

---
+++ AWS/handler.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const emojis = [
+'😊','😄','😉','😘','😍','💛','💜','❤️️','💖'
+];
+
+module.exports.rank = async event => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length-1 : rank];
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: rankEmoji,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
